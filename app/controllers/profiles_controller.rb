f257da5bfe9ae92b80975e9ba3e5f553234b4d50
Snuggs <rashaun.stovall@clash-media.com>
2011-07-27 16:28:44 -0400

    Refactor create action to not need keys explicitly stated for print

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,5 @@ class ProfilesController < ApplicationController
   def new;end
   
   def create
-    puts params.keys.join(', ')
-    @keys = [:first_name, :last_name, :email]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def display_params keys
-    keys.inject('') do |result, key|
-      result << "<p>#{key.to_s.titleize}: #{ params[key] }</p>" if params.keys.include? key
-    end
+    params.except(:controller, :action).
+        keys.inject('') do |result, key|
+          result << "<p>#{key.to_s.titleize}: #{ params[key] }</p>" if params.keys.include? key
+        end
   end
 end

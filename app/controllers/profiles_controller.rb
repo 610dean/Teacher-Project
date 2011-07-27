ee871d3115d35fab6b6bc3df6bbd45d061f1109f
Snuggs <rashaun.stovall@clash-media.com>
2011-07-27 16:20:57 -0400

    Refactor display_params to be a helper method

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,14 +2,7 @@ class ProfilesController < ApplicationController
   def new;end
   
   def create
-    render :text => display_params( :first_name, :last_name, :email )
-  end
-
-private
-
-  def display_params *keys
-    keys.inject('') do |result, key|
-      result << "<p>#{key.to_s.titleize}: #{ params[key] }</p>\n" if params[key]
-    end
+    puts params.keys.join(', ')
+    @keys = [:first_name, :last_name, :email]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def display_params keys
+    keys.inject('') do |result, key|
+      result << "<p>#{key.to_s.titleize}: #{ params[key] }</p>" if params.keys.include? key
+    end
+  end
 end
